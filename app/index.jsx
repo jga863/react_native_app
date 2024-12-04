c954aec6f7b81e0b7d41f4bb54ddc880264aeeed
JeffAR <jga863@ensign.edu>
2024-12-03 22:39:37 -0700

    complete-setup-ready

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+
+import {Link} from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+//import { Text, View } from 'react-native-web'; //If we want to work on it on web
+import { View, Text } from 'react-native';
+
+export default function App(){
+
+return (
+    <View className ="flex-1 items-center justify-center bg-white">
+        <Text className = "text-3x1">Aora!</Text>
+        <Text>Open up App.js to start working on your app! yo yo INDEX</Text>
+        <StatusBar style="auto" />
+        <Link href="/profile">Go to profile</Link>
+    </View>
+    );
+};
+
+
+
+
+
